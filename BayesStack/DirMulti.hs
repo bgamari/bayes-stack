03ea726a13fddf8ae8397aced3ac96282744217b
Ben Gamari <bgamari@gmail.com>
2012-01-14 21:42:02 -0500

    DirMulti hyperparameter estimation

--- BayesStack/DirMulti.hs
+++ BayesStack/DirMulti.hs
@@ -2,6 +2,8 @@
 
 module BayesStack.DirMulti ( -- * Dirichlet/multinomial pair
                              DirMulti(..), dirMulti, symDirMulti
+                           , dirMultiAlpha
+                           , estimatePrior
                            , decDirMulti, incDirMulti
                            , prettyDirMulti
                            ) where
@@ -10,10 +12,11 @@ import qualified Data.Foldable
 
 import Data.EnumMap (EnumMap)
 import qualified Data.EnumMap as EM
-  
+
 import Data.Sequence (Seq)
 import qualified Data.Sequence as SQ
 
+import Data.Foldable (toList)
 import Data.List (sortBy)
 import Data.Function (on)
 
@@ -57,8 +60,25 @@ data DirMulti a = SymDirMulti { dmAlpha :: Alpha
                   deriving (Show, Eq, Generic)
 
 type Alpha = Double
+type Alphas a = EnumMap a Alpha
+type Mean a = EnumMap a Double
+type Precision = Double
 
 instance (Enum a, Serialize a) => Serialize (DirMulti a)
+
+dirMultiAlpha :: Enum a => DirMulti a -> a -> Alpha
+dirMultiAlpha (SymDirMulti {dmAlpha=alpha}) = const $ alpha
+dirMultiAlpha (DirMulti {dmAlphas=alphas}) = (alphas EM.!)
+
+alphasToMeanPrecision :: Alphas a -> (Mean a, Precision)
+alphasToMeanPrecision alphas = let prec = sum $ EM.elems alphas
+                               in (fmap (/prec) alphas, prec)
+
+meanPrecisionToAlphas :: (Mean a, Precision) -> Alphas a
+meanPrecisionToAlphas (mean,prec) = fmap (*prec) mean
+
+symDirMultiFromPrecision :: [a] -> Precision -> DirMulti a
+symDirMultiFromPrecision domain prec = symDirMulti (0.5*prec) domain
 
 -- | Create a symmetric Dirichlet/multinomial pair
 symDirMulti :: Alpha -> [a] -> DirMulti a
@@ -67,6 +87,35 @@ symDirMulti alpha range = SymDirMulti { dmAlpha = alpha
                                       , dmTotal = 0
                                       , dmDomain = SQ.fromList range
                                       }
+
+nEstimationIters = 1000
+
+-- | Estimate the prior alpha from a Dirichlet/multinomial
+-- Based on Andrew Mccallum's interpretation of Tom Minka's implementation
+estimatePrior :: Enum a => [DirMulti a] -> Alphas a
+estimatePrior dms =
+  let domain = toList $ dmDomain $ head dms
+      --binHist :: Enum a => EnumMap a (EnumMap Int Int)
+      binHist = EM.map (EM.fromListWith (+))
+                $ EM.fromListWith (++) $ do dm <- dms
+                                            (a,c) <- EM.toList $ dmCounts dm
+                                            return (a, [(c, 1)])
+      lengthHist :: EnumMap Int Int
+      lengthHist = EM.fromListWith (+) $ do dm <- dms
+                                            return (dmTotal dm, 1)
+      --f :: Enum a => EnumMap a Alpha -> EnumMap a Alpha
+      f alphas = let newAlpha :: EnumMap Int Int -> Alpha
+                     newAlpha hist = let (n,_) = EM.findMax hist
+                                         digammas n = scanl (\digamma i -> digamma + 1/(sum (EM.elems alphas) + realToFrac i - 1)) 0 [1..n]
+                                     in realToFrac $ sum
+                                        $ zipWith (\digamma i->realToFrac (EM.findWithDefault 0 i hist)
+                                                               * realToFrac digamma)
+                                        (digammas n) [1..n]
+                     --alpha' :: Enum a => a -> Alpha -> Alpha
+                     alpha' k alpha = alpha * newAlpha (binHist EM.! k) / newAlpha lengthHist
+                  in EM.mapWithKey alpha' alphas
+      alphas0 = EM.fromList $ zip domain $ map (dirMultiAlpha $ head dms) domain
+  in head $ drop nEstimationIters $ iterate f alphas0
 
 -- | Create an asymmetric Dirichlet/multinomial pair
 dirMulti :: Enum a => [(a,Alpha)] -> [a] -> DirMulti a
