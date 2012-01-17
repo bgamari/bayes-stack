285267bc2191b6e267d1c5d7572504130fcaf3d5
Ben Gamari <bgamari@gmail.com>
2012-01-17 18:28:55 -0500

    Rework UniqueKey

--- BayesStack/UniqueKey.hs
+++ BayesStack/UniqueKey.hs
@@ -1,6 +1,7 @@
 module BayesStack.UniqueKey where
 
 import Control.Monad.Trans.State
+import Control.Monad (liftM)
 
 import Data.Map (Map)
 import qualified Data.Map as M
@@ -9,20 +10,25 @@ import Data.Tuple
 
 -- | 'UniqueKey cat id' is a monad for a calculation of a mapping unique keys
 -- 'id' on categorical values 'cat' to 
-type UniqueKey cat id = State (Int, Map cat id)
+type UniqueKeyT cat id m = StateT (Int, Map cat id) m
 
-uniqueKey :: Ord cat => (Int -> id) -> cat -> UniqueKey cat id id
-uniqueKey f x =
+newUniqueKey :: (Monad m, Ord cat) => (Int -> id) -> cat -> UniqueKeyT cat id m id
+newUniqueKey f x =
   do (next, m) <- get
      if x `M.member` m then return $ m M.! x
                        else do put (next+1, M.insert x (f next) m)
                                return $ f next
 
-runUniqueKey :: Ord id => UniqueKey cat id b -> (b, Map id cat)
-runUniqueKey a =
-  evalState (do result <- a
-                (_, m) <- get
-                let invMap = M.fromList $ map swap $ M.toList m
-                return (result, invMap)
-            ) (0, M.empty)
+runUniqueKey :: (Monad m, Ord id) => UniqueKeyT cat id m b -> m b
+runUniqueKey = liftM fst . runUniqueKeyWithInvMap
+
+getInvMap :: (Monad m, Ord id) => UniqueKeyT cat id m (Map id cat)
+getInvMap = do (_, m) <- get
+               return $ M.fromList $ map swap $ M.toList m
+runUniqueKeyWithInvMap :: (Monad m, Ord id) => UniqueKeyT cat id m b -> m (b, Map id cat)
+runUniqueKeyWithInvMap a =
+  evalStateT (do result <- a
+                 invMap <- getInvMap
+                 return (result, invMap)
+             ) (0, M.empty)
 
