7f4aef4d4843c1e7611d713c6af6be5d761a839d
Ben Gamari <bgamari@gmail.com>
2012-01-17 17:07:57 -0500

    CrossValSTO: Add edges cross-validation

--- LibThing/CrossValSTO.hs
+++ LibThing/CrossValSTO.hs
@@ -42,10 +42,11 @@ main =
         Left e -> putStrLn e
         otherwise -> return ()
      let Right state = s
-
      userGroups <- readUserGroups
-     createDirectoryIfMissing False $ f++"-crossval-nodes"
      let groups = nub $ map snd userGroups
+
+     createDirectoryIfMissing False $ f++"-crossval-nodes"
+     createDirectoryIfMissing False $ f++"-crossval-edges"
      forM_ groups $ \g@(Group n) ->
        do f <- openFile (printf "%s-crossval-nodes/group%d" f n) WriteMode
           forM_ (stNodes $ msData state) $ \u ->
@@ -53,5 +54,16 @@ main =
                hPrintf f "%d" (if isMember then 1 else 0 :: Int)
                forM_ (zip [1..] $ S.toList $ stTopics $ msData state) $ \(i,t) ->
                  hPrintf f " %d:%f" (i::Int) (theta state u t)
+               hPutStr f "\n"
+
+     groups <- getGroups
+     forM_ (EM.toList groups) $ \(Group i, members) ->
+       do f <- openFile (printf "%s-crossval-edges/group%d" f i) WriteMode
+          forM_ (stFriendships $ msData state) $ \(Friendship (a,b)) ->
+            do let isMember = a `elem` members && b `elem` members
+               hPrintf f "%d" (if isMember then 1 else 0 :: Int)
+               forM_ (zip [1..] $ S.toList $ stTopics $ msData state) $ \(j,t) ->
+                 let lambda = msLambdas state EM.! Friendship (a,b)
+                 in hPrintf f " %d:%f" (j::Int) (prob lambda t)
                hPutStr f "\n"
 
