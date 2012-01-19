ab225143b06c6b7dc90a96d333a613049ece6f50
Ben Gamari <bgamari@gmail.com>
2012-01-18 21:01:57 -0500

    Rewrite UniqueKey (again)

--- BayesStack/UniqueKey.hs
+++ BayesStack/UniqueKey.hs
@@ -12,37 +12,63 @@ import qualified Data.Map as M
 import Data.Tuple
 import Data.Functor.Identity
 
--- | 'UniqueKey cat id' is a monad for a calculation of a mapping unique keys
--- 'id' on categorical values 'cat' to 
+data UKState cat id = UKState { ukNextIdx :: Int
+                              , ukFromCat :: Map cat id
+                              }
+
+type CatMap id = Int -> id
+
+-- | 'UniqueKey cat id' is a monad for a calculation to assign unique
+-- keys 'id' (e.g. an internal integer representation wrapped in a newtype) to
+-- categorical values 'cat'
 type UniqueKey cat id = UniqueKeyT cat id Identity
-type UniqueKeyT cat id m = StateT (Int, Map cat id) m
+newtype UniqueKeyT cat id m a = UniqueKeyT { unUniqueKeyT :: (StateT (UKState cat id) (ReaderT (CatMap id) m) a) }
 
-newUniqueKey :: (Monad m, Ord cat) => (Int -> id) -> cat -> UniqueKeyT cat id m id
-newUniqueKey f x =
-  do (next, m) <- get
-     if x `M.member` m then return $ m M.! x
-                       else do put (next+1, M.insert x (f next) m)
-                               return $ f next
+instance Monad m => Monad (UniqueKeyT cat id m) where
+  -- UniqueKey a -> (a -> UniqueKey b) -> UniqueKey b
+  (UniqueKeyT m) >>= f  =  UniqueKeyT (m >>= \x -> unUniqueKeyT (f x))
+  return = UniqueKeyT . return
 
-runUniqueKey :: (Ord id) => UniqueKey cat id b -> b
-runUniqueKey = runIdentity . runUniqueKeyT
+instance MonadTrans (UniqueKeyT cat id) where
+  lift = UniqueKeyT . lift . lift
 
-runUniqueKeyT :: (Monad m, Ord id) => UniqueKeyT cat id m b -> m b
-runUniqueKeyT a = evalStateT a (0, M.empty)
+instance MonadIO m => MonadIO (UniqueKeyT cat id m) where
+  liftIO = lift . liftIO
 
-getInvMap :: (Monad m, Ord id) => UniqueKeyT cat id m (Map id cat)
-getInvMap = do (_, m) <- get
-               return $ M.fromList $ map swap $ M.toList m
+newUniqueKey :: (Monad m, Ord cat) => cat -> UniqueKeyT cat id m id
+newUniqueKey x = UniqueKeyT $
+  do UKState {ukNextIdx=next, ukFromCat=from} <- get
+     catmap <- lift $ ask
+     if x `M.member` from
+       then return $ from M.! x
+       else do let c = catmap next
+               put $ UKState { ukNextIdx = next+1
+                             , ukFromCat = M.insert x c from
+                             }
+               return c
 
-runUniqueKeyTWithInvMap :: (Monad m, Ord id) => UniqueKeyT cat id m b -> m (b, Map id cat)
-runUniqueKeyTWithInvMap a =
-  runUniqueKeyT $ do result <- a
-                     invMap <- getInvMap
-                     return (result, invMap)
+runUniqueKey :: (Ord id) => CatMap id -> UniqueKey cat id b -> b
+runUniqueKey catmap m = runIdentity $ runUniqueKeyT catmap m
 
-runUniqueKeyWithInvMap :: (Ord id) => UniqueKey cat id b -> (b, Map id cat)
-runUniqueKeyWithInvMap a =
-  runUniqueKey $ do result <- a
-                    invMap <- getInvMap
-                    return (result, invMap)
+runUniqueKeyT :: (Monad m, Ord id) => CatMap id -> UniqueKeyT cat id m b -> m b
+runUniqueKeyT catmap (UniqueKeyT a) = runReaderT (evalStateT a initial) catmap
+  where initial = UKState { ukNextIdx = 0, ukFromCat = M.empty }
+
+getMapToCat :: (Monad m, Ord id) => UniqueKeyT cat id m (Map id cat)
+getMapToCat = UniqueKeyT $ liftM (M.fromList . map swap . M.toList . ukFromCat) $ get
+
+getMapFromCat :: (Monad m, Ord id) => UniqueKeyT cat id m (Map cat id)
+getMapFromCat = UniqueKeyT $ liftM ukFromCat get
+
+runUniqueKeyTWithInvMap :: (Monad m, Ord id) => CatMap id -> UniqueKeyT cat id m b -> m (b, Map id cat)
+runUniqueKeyTWithInvMap catmap a =
+  runUniqueKeyT catmap $ do result <- a
+                            invMap <- getMapToCat
+                            return (result, invMap)
+
+runUniqueKeyWithInvMap :: (Ord id) => CatMap id -> UniqueKey cat id b -> (b, Map id cat)
+runUniqueKeyWithInvMap catmap a =
+  runUniqueKey catmap $ do result <- a
+                           invMap <- getMapToCat
+                           return (result, invMap)
 
