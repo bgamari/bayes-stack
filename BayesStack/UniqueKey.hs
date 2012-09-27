fc18f9b0876427a56ea25d3281a1e985da1162e0
Ben Gamari <bgamari.foss@gmail.com>
2012-09-27 15:37:27 -0400

    UniqueKey: Complete rewrite

--- BayesStack/UniqueKey.hs
+++ BayesStack/UniqueKey.hs
@@ -1,48 +1,77 @@
-module BayesStack.UniqueKey where
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+    
+module BayesStack.UniqueKey ( getUniqueKey
+                            , getValueMap, getKeyMap
+                            , mapTraversable
+                            , UniqueKey, runUniqueKey
+                            , UniqueKeyT, runUniqueKeyT
+                            ) where
 
-import Control.Monad.Trans.Class
-import Control.Monad.IO.Class
-import Control.Monad.Trans.Reader
-import Control.Monad.Trans.State
-import Control.Monad (liftM)
-
-import Data.Map (Map)
-import qualified Data.Map as M
-
+import Prelude hiding (mapM)       
+import Control.Applicative (Applicative, (<$>))       
+import Data.Traversable (Traversable, mapM)
 import Data.Tuple
 import Data.Functor.Identity
 
--- | 'UniqueKey cat id' is a monad for a calculation of a mapping unique keys
--- 'id' on categorical values 'cat' to 
-type UniqueKey cat id = UniqueKeyT cat id Identity
-type UniqueKeyT cat id m = StateT (Int, Map cat id) m
+import Control.Monad.Trans
+import Control.Monad.State hiding (mapM)
+import Control.Monad (liftM)
 
-newUniqueKey :: (Monad m, Ord cat) => (Int -> id) -> cat -> UniqueKeyT cat id m id
-newUniqueKey f x =
-  do (next, m) <- get
-     if x `M.member` m then return $ m M.! x
-                       else do put (next+1, M.insert x (f next) m)
-                               return $ f next
+       
+import Data.Map (Map)
+import qualified Data.Map as M
 
-runUniqueKey :: (Ord id) => UniqueKey cat id b -> b
-runUniqueKey = runIdentity . runUniqueKeyT
+-- | 'UniqueKey val key' is a monad for a calculation of a mapping unique keys
+-- 'key' onto values 'val'
+type UniqueKey val key = UniqueKeyT val key Identity
+newtype UniqueKeyT val key m a = UniqueKeyT (StateT ([key], Map val key) m a)
+                              deriving (Monad, Applicative, Functor, MonadTrans)
 
-runUniqueKeyT :: (Monad m, Ord id) => UniqueKeyT cat id m b -> m b
-runUniqueKeyT a = evalStateT a (0, M.empty)
+-- | Get map of unique keys to values              
+getKeyMap :: (Monad m, Applicative m, Ord key, Ord val) => UniqueKeyT val key m (Map key val)
+getKeyMap = M.fromList . map swap . M.toList <$> getValueMap
 
-getInvMap :: (Monad m, Ord id) => UniqueKeyT cat id m (Map id cat)
-getInvMap = do (_, m) <- get
-               return $ M.fromList $ map swap $ M.toList m
+-- | Get map of values to unique keys
+getValueMap :: (Monad m, Applicative m, Ord key, Ord val) => UniqueKeyT val key m (Map val key)
+getValueMap = snd <$> UniqueKeyT get
 
-runUniqueKeyTWithInvMap :: (Monad m, Ord id) => UniqueKeyT cat id m b -> m (b, Map id cat)
-runUniqueKeyTWithInvMap a =
-  runUniqueKeyT $ do result <- a
-                     invMap <- getInvMap
-                     return (result, invMap)
+popUniqueKey :: Monad m => UniqueKeyT val key m key
+popUniqueKey = do
+    (keys, a) <- UniqueKeyT get
+    case keys of
+        key:rest -> UniqueKeyT (put (rest, a)) >> return key
+        []      -> error "Ran out of unique keys"
+    
+-- | Find the unique key for value 'val' or 'Nothing' if the value is unknown
+findUniqueKey :: (Monad m, Applicative m, Ord key, Ord val) => val -> UniqueKeyT val key m (Maybe key)
+findUniqueKey value = M.lookup value <$> getValueMap
 
-runUniqueKeyWithInvMap :: (Ord id) => UniqueKey cat id b -> (b, Map id cat)
-runUniqueKeyWithInvMap a =
-  runUniqueKey $ do result <- a
-                    invMap <- getInvMap
-                    return (result, invMap)
+getUniqueKey :: (Monad m, Applicative m, Ord key, Ord val) => val -> UniqueKeyT val key m key
+getUniqueKey x = do
+    key <- findUniqueKey x
+    case key of
+        Just k  -> return k
+        Nothing -> do k <- popUniqueKey
+                      UniqueKeyT $ modify $ \(keys, keyMap)->(keys, M.insert x k keyMap)
+                      return k
 
+runUniqueKey :: (Ord key) => [key] -> UniqueKey val key a -> a
+runUniqueKey keys = runIdentity . runUniqueKeyT keys
+
+runUniqueKeyT :: (Monad m, Ord key) => [key] -> UniqueKeyT val key m a -> m a
+runUniqueKeyT keys (UniqueKeyT a) = evalStateT a (keys, M.empty)
+
+runUniqueKeyTWithInvMap :: (Monad m, Applicative m, Ord key, Ord val) => [key] -> UniqueKeyT val key m a -> m (a, Map key val)
+runUniqueKeyTWithInvMap keys action =
+    runUniqueKeyT keys $ do result <- action
+                            keyMap <- getKeyMap
+                            return (result, keyMap)
+
+runUniqueKeyWithInvMap :: (Ord key, Ord val) => [key] -> UniqueKey val key a -> (a, Map key val)
+runUniqueKeyWithInvMap keys action =
+    runUniqueKey keys $ do result <- action
+                           keyMap <- getKeyMap
+                           return (result, keyMap)
+
+mapTraversable :: (Traversable t, Ord key, Ord val) => [key] -> t val -> (t key, Map key val)
+mapTraversable keys xs = runUniqueKeyWithInvMap keys $ mapM getUniqueKey xs
